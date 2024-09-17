ebb3bffcf415f9698800b960157cc504951097a3
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2024-09-17 11:19:26 -0700

    first pass

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,7 +1,55 @@
-public class MyMain {
-    public static void main(String[] args) {
-        MyClass myClass = new MyClass("Hello World!");
+import java.util.Scanner;
 
-        System.out.println(myClass.getGreeting());
+public class MyMain {
+
+    // Counts the number of 'a', 'b', and 'c' chars in str
+    public static int countABC(String str) {
+        // REPLACE WITH YOUR CODE
+        return -1;
+    }
+
+    // Checks to see whether or not str contains 'The' or 'the'
+    public static boolean containsThe(String str) {
+        // REPLACE WITH YOUR CODE
+        return false;
+    }
+
+    // Checks whether str is a palindrome or not
+    public static boolean isPalindrome(String str) {
+        // REPLACE WITH YOUR CODEE
+        return false;
+    }
+
+
+    // Takes two Strings as input, and returns a String equal to the first half
+    // of the shorter String plus the second half of the longer String.
+    public static String halfAdder(String str1, String str2) {
+        // REPLACE THIS WITH YOUR CODE
+        return "";
+    }
+
+    //  Takes in two Strings as input and returns a String equal to those two Strings added
+    //  together in alphabetic order.
+    public static String stringOrder(String str1, String str2) {
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+        // Some code to test halfAdder
+        System.out.println("halfAdder tests:");
+        System.out.println(halfAdder("computer","science")); // sciuter
+        System.out.println(halfAdder("apple","fig")); // fple
+        System.out.println(halfAdder("banana","boat")); // boana
+        System.out.println(halfAdder("crablike","pineapple")); // crabapple
+        System.out.println(halfAdder("trombone", "hippie")); // hipbone
+
+        // Some code to test stringOrder
+        System.out.println("\nstringOrder tests:");
+        System.out.println(stringOrder("apple", "banana")); // applebanana
+        System.out.println(stringOrder("banana", "apple")); // applebanana
+        System.out.println(stringOrder("cranberry", "banana")); // bananacranberry
+        System.out.println(stringOrder("apple", "Banana")); // appleBanana
+        System.out.println(stringOrder("Cranberry", "apple")); // appleCranberry
     }
 }
