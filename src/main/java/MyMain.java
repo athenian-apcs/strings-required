17f2437d36b6eeb7fa9eb5eecbd37588fcff2c15
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2024-09-26 13:17:08 -0700

    Wrote javadocs for every method, chose methods to be required, wrote tests for all methods.

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,54 +2,67 @@ import java.util.Scanner;
 
 public class MyMain {
 
-    // Counts the number of 'a', 'b', and 'c' chars in str
-    public static int countABC(String str) {
-        // REPLACE WITH YOUR CODE
-        return -1;
-    }
-
-    // Checks to see whether or not str contains 'The' or 'the'
+    /**
+     * Checks whether a given string contains the word "the" with any capitalization.
+     * @param str The string to be checked. Not null. Contains only letters and spaces, and
+     *            follows grammar rules.
+     * @return true if the string contains "the", false otherwise
+     */
     public static boolean containsThe(String str) {
         // REPLACE WITH YOUR CODE
         return false;
     }
 
-    // Checks whether str is a palindrome or not
+    /**
+     * Counts the number of 'a', 'b', and 'c' chars in str.
+     * @param str The string to be checked. Not null. Only contains lowercase letters.
+     * @return The number of matching characters.
+     */
+    public static int countABC(String str) {
+        // REPLACE WITH YOUR CODE
+        return -1;
+    }
+
+    /**
+     * Checks whether str is a palindrome or not.
+     * @param str The string to be checked. Not null. Only contains lowercase letters.
+     * @return true if the string is a palindrome, false otherwise.
+     */
     public static boolean isPalindrome(String str) {
-        // REPLACE WITH YOUR CODEE
+        // REPLACE WITH YOUR CODE
         return false;
     }
 
-
-    // Takes two Strings as input, and returns a String equal to the first half
-    // of the shorter String plus the second half of the longer String.
-    public static String halfAdder(String str1, String str2) {
-        // REPLACE THIS WITH YOUR CODE
-        return "";
-    }
-
-    //  Takes in two Strings as input and returns a String equal to those two Strings added
-    //  together in alphabetic order.
+    /**
+     * Takes in two Strings as input and returns a String equal to those two Strings added together
+     * in alphabetic order.
+     * <p>
+     * Example: <code>stringOrder("banana", "apple")</code> should return <code>"applebanana"</code>.
+     * @param str1 One string to be added. Might not be first alphabetically. Not null.
+     * @param str2 One string to be added. Might not be second alphabetically. Not null.
+     * @return The two strings added together.
+     */
     public static String stringOrder(String str1, String str2) {
+        // REPLACE WITH YOUR CODE
         return "";
     }
 
+    /**
+     * Takes two Strings as input, and returns a String equal to the first half
+     * of the shorter String plus the second half of the longer String. The halved length
+     * of the shorter String should be rounded down, but the halved length of the longer
+     * String should be rounded up.
+     * <p>
+     * Example: <code>halfAdder("apple", "fig")</code> should return <code>"fple"</code>.
+     * @param str1 One string to be half-added. Might not be shorter. Not null.
+     * @param str2 One string to be half-added. Might not be longer. Not null.
+     * @return The two strings smashed together.
+     */
+    public static String halfAdder(String str1, String str2) {
+        // REPLACE WITH YOUR CODE
+        return "";
+    }
 
     public static void main(String[] args) {
-        // Some code to test halfAdder
-        System.out.println("halfAdder tests:");
-        System.out.println(halfAdder("computer","science")); // sciuter
-        System.out.println(halfAdder("apple","fig")); // fple
-        System.out.println(halfAdder("banana","boat")); // boana
-        System.out.println(halfAdder("crablike","pineapple")); // crabapple
-        System.out.println(halfAdder("trombone", "hippie")); // hipbone
-
-        // Some code to test stringOrder
-        System.out.println("\nstringOrder tests:");
-        System.out.println(stringOrder("apple", "banana")); // applebanana
-        System.out.println(stringOrder("banana", "apple")); // applebanana
-        System.out.println(stringOrder("cranberry", "banana")); // bananacranberry
-        System.out.println(stringOrder("apple", "Banana")); // appleBanana
-        System.out.println(stringOrder("Cranberry", "apple")); // appleCranberry
     }
 }
